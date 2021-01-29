0a04543a8405a0052cf112e792e79ae3bd3e3665
Jashaja18 <67085053+Jashaja18@users.noreply.github.com>
2021-01-28 22:55:03 -0800

    Update ShoppingCart.java

--- ShoppingCart.java
+++ ShoppingCart.java
@@ -1,4 +1,3 @@
-
 /*
  * Shreya Karnati
  * Group #4
@@ -9,50 +8,77 @@
 
 import java.util.*;
 
-public class ShoppingCart {
+public class ShoppingCart{
 
-	// Creating the array list used
-	private ArrayList<ItemOrder> cart = new ArrayList<>();
-	
-	public ShoppingCart() {
-		
-	}
-	
-	public double getTotalPrice() {
+	private ArrayList<ItemOrder> fullCart = new ArrayList<ItemOrder>();
+
+	// default constuctor
+	public ShoppingCart() {}
+
+	// this will get the total cost of the cart
+	public double getTotalPrice()
+	{
 		double total = 0.0;
-		for (int i = 0; i < cart.size(); i++) {
-			ItemOrder order = cart.get(i);
-			total += order.getOrderPrice();
+		for (int i = 0; i < fullCart.size(); i++)
+		{
+			ItemOrder item = fullCart.get(i);
+			total += item.getOrderTotal();
 		}
 		return total;
 	}
-	
-	public void add (ItemOrder order) {
-		cart.add(order);
-	}
-	
-	public void remove (ItemOrder order) {
-		cart.remove(order);
-	}
-	
-	public boolean search(ItemOrder Order) {
-	  boolean found = false;
-	  
-	   for(int index = 0; index < cart.size(); index++) {
-		   
-		if (Order.equals(cart.get(index)))
-		found = true;
-		
-	   }
-	    return found;
+
+	public void addItemToCart(ItemOrder orderIt)
+	{
+		fullCart.add(orderIt);
 	}
 
-	// Printing out all of the item orders
-	public void printOrders()
-    {
-        for (int i = 0; i < cart.length(); i++)
-        {
-            System.out.println(cart[i].toString() + " " + cart[i].getCost());
-        }
-    }
+	public void removeFromCart(ItemOrder orderIt)
+	{
+		fullCart.remove(orderIt);
+	}
+
+	// This is used to search the item in the cart
+	public boolean searchCart(ItemOrder orderSearch)
+	{
+		boolean itemFound = false;
+		for(int j = 0; j < fullCart.size(); j++)
+		{
+			if (orderSearch.equals(fullCart.get(j)))
+			{
+				itemFound = true;
+			}
+		}
+		return itemFound;
+	}
+
+	// standard Java toString
+	public void printCart(ShoppingCart cart)
+	{
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Your cart: ");
+		for(ItemOrder i: fullCart)
+		{
+			System.out.print(i.getQuantity() + " units " + i.getItem().getItemName());
+			for (int x = 20; x > i.getItem().getItemName().length(); x--) 
+			{
+				System.out.print(".");
+			}
+			
+			System.out.print("$" + i.getOrderTotal());
+			System.out.println();
+
+			/*        	if (i.getQuantity() < 10) {
+        		System.out.println("Would you like to purchase " + (10 - i.getQuantity()) + 
+        				" more units to receive a 25% discount on the 10 units? (Reply y/n)");
+        		String answer = input.nextLine();
+        		if (answer == "y") {
+        			i.setallQuantity(10);
+        		}
+        	}
+			 */
+
+		}
+		System.out.println("Total Price: $" + cart.getTotalPrice());
+	}
 }
