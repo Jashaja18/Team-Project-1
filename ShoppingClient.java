1f9f6f1f1764065e85aefac36742b5143cc1f25a
DonSama <77746863+DonSama@users.noreply.github.com>
2021-01-29 01:01:29 -0800

    Update ShoppingClient.java

--- ShoppingClient.java
+++ ShoppingClient.java
@@ -15,16 +15,27 @@ public class ShoppingCartClient {
         
         Item item1 = new Item("cookies", 5.00);
         Item item2 = new Item("milk", 6.00);
+        Item item3 = new Item("butter", 2.95);
+        Item item4 = new Item("tomatoes", 0.99);
+        Item item5 = new Item("asparagus", 1.31);
         
         // Create grocery items with the name, price per unit,
-        // and quantity per unit to account for bulk purchases
+        // and quantity
         ItemOrder order1 = new ItemOrder(item1, 1);
         ItemOrder order2 = new ItemOrder(item2, 2);
+        ItemOrder order3 = new ItemOrder(item3, 12);
+        ItemOrder order4 = new ItemOrder(item4, 5);
+        ItemOrder order5 = new ItemOrder(item5, 3);
 
         // Add grocery items to the grocery list object
         myCart.addItemToCart(order1);
         myCart.addItemToCart(order2);
+        myCart.addItemToCart(order3);
+        myCart.addItemToCart(order4);
+        myCart.addItemToCart(order5);
         
+        // Print the information about what's in our cart
+        // and how much it costs
         myCart.printCart(myCart);
         
     }
